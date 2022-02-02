7f45abb0b148c8da6ffbf29347d2fbcc98abd9a2
daksh-2020 <85994350+daksh-2020@users.noreply.github.com>
2022-02-02 15:57:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+
+var last_x, last_y;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    var width=screen.width
+    color = "black";
+    width_of_line = 2;
+
+    
+    new_width = screen.width- 70
+    new_height = screen.height- 300;
+    if(width<992)
+    {
+        document.getElementById("myCanvas").width = new_width;
+        document.getElementById("myCanvas").height =new_height;
+        document.body.style.overflow = "hidden";
+    }
+    canvas.addEventListener("touchstart", my_touchstart);
+    
+    function my_touchstart(e)
+    
+    {
+        console.log("my_touchstart");
+        //Addictonal Activity start
+        color = document.getElementById("color").value;
+        width_of_line = document.getElementById("width_of_line").value;
+        //Addictonal Activity ends
+
+        last_x = e.touches[0].clientX- canvas.offsetLeft;
+        last_y = e.touches[0].clientY - canvas.offsetTop;
+    }
+
+
+    canvas.addEventListener("touchmove", my_touchmove);
+    function my_touchmove(e) 
+    {
+    
+        console.log("my_touchMove");
+        current_x = e.touches[0].clientX - canvas.offsetLeft;
+        current_y = e.touches[0].clientY - canvas.offsetTop;
+    
+        // old same old as the paint web app
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+    
+        console.log("Last position of x and y coordinates = ");
+        console.log("x = " + last_x + "y = " + last_y);
+        ctx.moveTo(last_x, last_y);
+    
+        console.log("Current position of x and y coordinates = ");
+        console.log("x  = " + current_x + "y = " + current_y);
+        ctx.lineTo(current_x, current_y);
+        ctx.stroke();
+    
+        last_x = current_x; 
+        last_y = current_y;
+        
+        // end of old same old as the paint web app
+    }
+    
+        // old same old as the paint web app
+    function clearArea() {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    }
+        // end of old same old as the paint web app
